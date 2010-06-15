efb92e6221b36eae5699ad7f47a98a205c18bd13
Roman Cheplyaka <roma@ro-che.info>
2010-06-15 09:21:30 +0300

    Modify loker.hs to dump parse tree and state

--- loker.hs
+++ loker.hs
@@ -2,14 +2,18 @@ module Main where
 import System.Environment
 import System.Exit
 import Parse
-import Parsec (parse)
+import Parsec
 
 main = do
     args <- getArgs
     s <- if null args 
             then getContents
             else readFile (head args)
-    let res = parse program (if null args then "" else head args) s
+    let program_and_state = do
+            p <- program
+            s <- getState
+            return (p,s)
+    let res = parse program_and_state (if null args then "" else head args) s
     case res of
-        Right _ -> do putStrLn "OK"; exitSuccess
+        Right s -> do print s; exitSuccess
         Left m  -> do print m; exitFailure
