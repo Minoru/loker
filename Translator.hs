041ebe8ae7108951ddf8d0213644f811e178b261
Andreo <andrey.torba@gmail.com>
2010-10-16 16:18:44 +0300

    cosmetic changes in Translator.sh

--- Translator.hs
+++ Translator.hs
@@ -4,7 +4,7 @@ module Translator where
 import AST
 import IR
 
-parseAssignment (Assignment name (Bare value:[])) = Const (UserVariable name 0) value
+translateAssignment (Assignment name [Bare value]) = Const (UserVariable name 0) value
 
 -- simpleCommantToInstructions :: SimpleCommand -> [Instruction]
 -- simpleCommantToInstructions (SimpleCommand ax [] []) = parseAssignments ax
