60d6dd6896cb9b381134e342b1f4a7fa043c609a
Andreo <andrey.torba@gmail.com>
2010-10-16 16:18:07 +0300

    add deriving (Show,Read,Eq) to data types

--- IR.hs
+++ IR.hs
@@ -5,15 +5,20 @@ module IR where
 data StringValue
     = ValVariable Variable
     | ValExitStatus ExitStatusUnique
+      deriving (Show,Read,Eq)
 
 data Variable
     = UserVariable String Int -- ordinary shell variable
     | InternalVariable InternalVariableUnique -- once-assigned variable introduced during translation
+      deriving (Show,Read,Eq)
 
 data Array = ArrayUnique Int
+      deriving (Show,Read,Eq)
 
 newtype ExitStatusUnique = ExitStatusUnique Int
+      deriving (Show,Read,Eq)
 newtype InternalVariableUnique = InternalVariableUnique Int
+      deriving (Show,Read,Eq)
 
 data Instruction
     = Execute
@@ -36,13 +41,17 @@ data Instruction
         ExitStatusUnique
         Instruction
         Instruction  
+      deriving (Show,Read,Eq)
+
 
 data SpecialBuiltin
     = Break | Colon | Continue | Dot | Eval | Exec | Exit | Export
     | Readonly | Return | Set | Shift | Times | Trap | Unset
+      deriving (Show,Read,Eq)
 
 data Builtin
     = Alias | Bg | Cd | Command | False | Fc | Fg | Getopts | Jobs
     | Kill | Newgrp | Pwd | Read | True | Umask | Unalias | Wait
+      deriving (Show,Read,Eq)
 
 type Script = [Instruction]
