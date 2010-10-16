d04db70605364edb99fb1c902e6dd327c76594e0
Roman Cheplyaka <roma@ro-che.info>
2010-10-16 16:43:29 +0300

    Add UniqueM

--- Translator.hs
+++ Translator.hs
@@ -4,7 +4,23 @@ module Translator where
 import AST
 import IR
 
-translateAssignment (Assignment name [Bare value]) = Const (UserVariable name 0) value
+import Control.Monad.State
+
+type UniqueM = State Int
+
+newUnique :: UniqueM Int
+newUnique = do
+    i <- get
+    put (i+1)
+    return i
+
+runUniqueM :: UniqueM a -> a
+runUniqueM m = evalState m 0
+
+translateAssignment :: Assignment -> UniqueM [Instruction]
+translateAssignment (Assignment name [Bare value]) = do
+    i <- newUnique
+    return [Const (UserVariable name i) value]
 
 -- simpleCommantToInstructions :: SimpleCommand -> [Instruction]
 -- simpleCommantToInstructions (SimpleCommand ax [] []) = parseAssignments ax
