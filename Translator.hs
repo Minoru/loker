65eeef8fbeb82b1210ccd53d47c57ddab2964dd9
Andreo <andrey.torba@gmail.com>
2010-10-17 13:21:31 +0300

    add translateParameter

--- Translator.hs
+++ Translator.hs
@@ -5,24 +5,33 @@ import AST
 import IR
 
 import Control.Monad.State
+import qualified Data.Map as M
 
-type UniqueM = State Int
+type UniqueM = State (M.Map String Int)
 
-newUnique :: UniqueM Int
-newUnique = do
-    i <- get
-    put (i+1)
+newVarVersion :: String -> UniqueM Int
+newVarVersion name = do
+    m <- get
+    let i = M.findWithDefault 0 name m
+    put (M.insert name (i+1) m)
     return i
 
+newUniqueNumber :: UniqueM Int  -- number for internal variables
+newUniqueNumber = newVarVersion ""
+
 runUniqueM :: UniqueM a -> a
-runUniqueM m = evalState m 0
+runUniqueM m = evalState m M.empty
 
 translateAssignment :: Assignment -> UniqueM [Instruction]
 translateAssignment (Assignment name [Bare value]) = do
-    i <- newUnique
-    return [IR.Const (UserVariable name i) value]
+    i <- newVarVersion name
+    return [IR.Const (UserVariable name 0) value]
 
 translateSimpleCommand :: SimpleCommand -> UniqueM [Instruction]
 translateSimpleCommand (SimpleCommand ax [] [])
     = liftM concat (mapM translateAssignment ax)
 
+translateParameter :: Parameter -> UniqueM Variable
+translateParameter (Var name) = do
+  i <- newVarVersion name
+  return (UserVariable name i)
