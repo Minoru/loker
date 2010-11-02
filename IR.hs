a6a73cabe20a7e229659c5535b3aa799599ba897
Roman Cheplyaka <roma@ro-che.info>
2010-11-02 09:39:50 +0200

    Add simplification to IR

--- IR.hs
+++ IR.hs
@@ -93,3 +93,85 @@ translateSimpleCommand (AST.SimpleCommand as rs ws) =
     in if null redirections
         then cmd
         else ApplyRedirections redirections cmd
+
+----------------------------------------
+-- Simplifications of IR tree
+
+-- todo: replace with attribute grammar, syb-like or something else
+
+simplifyStatement :: Statement -> Statement
+simplifyStatement (Command args as) =
+    Command (simplifyArray args) (map simplifyAssignment as)
+simplifyStatement (Sequence [s]) = simplifyStatement s
+simplifyStatement (Sequence ss) = Sequence $ map simplifyStatement ss
+simplifyStatement (ApplyRedirections r s) =
+    ApplyRedirections (map simplifyRedirection r) (simplifyStatement s)
+simplifyStatement (Pipeline [s]) = simplifyStatement s
+simplifyStatement (Pipeline ss) = Pipeline $ map simplifyStatement ss
+
+simplifyArray :: Array -> Array
+simplifyArray (Split e) =
+    let (e',f) = simplifyExpression e
+    in if splittable f
+        then Split e'
+        else Field e'
+simplifyArray (Glob e) =
+    let (e',f) = simplifyExpression e
+    in if globbable f
+        then Glob e'
+        else Field e'
+simplifyArray (SplitGlob e) =
+    let (e',f) = simplifyExpression e
+    in case f of
+        Fact { splittable = True,  globbable = True  } -> SplitGlob e'
+        Fact { splittable = False, globbable = True  } -> Glob e'
+        Fact { splittable = True,  globbable = False } -> Split e'
+        Fact { splittable = False, globbable = False } -> Field e'
+simplifyArray (Field e) =
+    let (e',f) = simplifyExpression e
+    in Field e'
+simplifyArray (ConcatA [a]) = simplifyArray a
+simplifyArray (ConcatA as) = ConcatA $ map simplifyArray as
+
+data Fact = Fact
+    { globbable  :: Bool
+    , splittable :: Bool
+    , mayContainPattern :: Bool
+    }
+
+allFalse :: Fact
+allFalse = Fact
+    { globbable = False
+    , splittable = False
+    , mayContainPattern = False
+    }
+
+simplifyExpression :: Expression -> (Expression, Fact)
+simplifyExpression x@Parameter{} = (x, allFalse { mayContainPattern = True })
+simplifyExpression x@(Const e)     = (x, allFalse { mayContainPattern = pat })
+  where
+    pat = any (`elem` e) "*?["
+simplifyExpression x@(Splittable e) =
+    let (e',f) = simplifyExpression e
+    in  (Splittable e', f { splittable = True })
+simplifyExpression x@(Globbable e) =
+    let (e',f) = simplifyExpression e
+    in if mayContainPattern f
+        then (Globbable e', f { globbable = True })
+        else (e',           f { globbable = False })
+simplifyExpression x@(ConcatE [e]) = simplifyExpression e
+simplifyExpression x@(ConcatE es) =
+    let (es', fs) = unzip $ map simplifyExpression es
+        f' = Fact
+            { splittable = any splittable fs
+            , globbable = any globbable fs
+            , mayContainPattern = any mayContainPattern fs}
+    in  (ConcatE es', f')
+
+simplifyAssignment :: Assignment -> Assignment
+simplifyAssignment (Assignment name expr) =
+    Assignment name $ fst $ simplifyExpression expr
+
+simplifyRedirection :: Redirection -> Redirection
+simplifyRedirection (Redirection fd op expr) =
+    Redirection fd op $ fst $ simplifyExpression expr
