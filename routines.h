e3b5bbbbdd957258a1f416a9b2557aaf9d913e50
Alexander Batischev <eual.jp@gmail.com>
2011-09-18 02:52:33 +0300

    AND and OR macros added to routines.h

--- routines.h
+++ routines.h
@@ -3,6 +3,28 @@
 
 #include <stddef.h> /* NULL */
 
+#define AND(CMD1, STATUS1, LABEL, CMD2, RETVAL) \
+    STATUS1 = CMD1; \
+    if(STATUS1 != 0) \
+    { \
+        RETVAL = STATUS1; \
+        goto LABEL; \
+    } \
+    \
+    RETVAL = CMD2; \
+    LABEL : ;
+
+#define OR(CMD1, STATUS1, LABEL, CMD2, RETVAL) \
+    STATUS1 = CMD1; \
+    if(STATUS1 == 0) \
+    { \
+        RETVAL = STATUS1; \
+        goto LABEL; \
+    } \
+    \
+    RETVAL = CMD2; \
+    LABEL : ;
+
 int exec_command(char **);
 int pipeline(int, char***);
 
